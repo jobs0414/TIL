7608e58abefb00dc807d3a100d8cbb9d89c4e7a7
jobs0414 <tkddn262@naver.com>
2018-10-30 21:24:02 +0900

    django - update

--- myboard/myboard/cms/urls.py
+++ myboard/myboard/cms/urls.py
@@ -9,4 +9,15 @@ urlpatterns = [
     path('book/add/', views.book_edit, name='book_add'),
     path('book/modify/<int:book_id>/', views.book_edit, name='book_modify'),
     path('book/delete/<int:book_id>/', views.book_remove, name='book_remove'),
+
+    path('impression/<int:book_id>', views.ImpressionList.as_view(), name='impression_list'),
+    path('impression/add/<int:book_id>', views.impression_edit, name='impression_add'),
+    path('impression/modify/<int:book_id>/<int:impression_id>', views.impression_edit, name='impression_modify'),
+    path('impression/delete/<int:book_id>/<int:impression_id>', views.impression_remove, name='impression_remove'),
+
+
+
+
+
+
 ]
--- myboard/myboard/myboard/urls.py
+++ myboard/myboard/myboard/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cms/',include('cms.urls')),
+    path('cms/', include('cms.urls')),
+    
+
+
+
+
 
 ]
